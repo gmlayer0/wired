8965323be7ef74f3f68672ae49eee541fdac289e
dofingert <dofingert@gmail.com>
2024-03-20 18:23:41 +0800

    继续完善 TL 侧状态机

--- rtl/wired2_tl_adapter.sv
+++ rtl/wired2_tl_adapter.sv
@@ -39,15 +39,25 @@ module wired_tl_adapter import tl_pkg::*; #(
     // --- prb call inv
     logic prb_inv_cal; // prb drive
     logic prb_inv_ret; // inv drive
+    typedef enum logic[2:0] {
+        PRB_HIT_ADDR_N, // for prb inv toN
+        PRB_HIT_ADDR_B, // for prb inv toB
+        RD_ALLOC,       // for read inv, 找到一个合适的行释放并返回
+        WR_ALLOC,       // for write miss, check whether read hit, if hit, just return.
+                        // otherwise, invalidate random way and return.
+        IDX_INV,        // INDEX INVALID WRITE BACK
+        IDX_INIT        // INDEXED INIT
+    } inv_parm_e;
     // payload
-    logic        prb_inv_hit;  // prb drive, 仅无效化命中行，否之无效化 addr[15:12] 掩码对应所有行
+    inv_parm_e   prb_inv_parm; // prb drive
     logic [31:0] prb_inv_addr; // prb drive
     // - crq begin
     // --- crq call inv
-    logic crq_inv_cal; // crq drive
-    logic crq_inv_ret; // inv drive
+    logic        crq_inv_cal; // crq drive
+    logic        crq_inv_ret; // inv drive
+    logic [1:0]  crq_inv_sel; // inv drive
     // payload
-    logic        prb_inv_hit;  // crq drive, 仅无效化命中行，否之无效化 addr[15:12] 掩码对应所有行
+    inv_parm_e   crq_inv_parm; // crq drive
     logic [31:0] crq_inv_addr; // crq drive
     // --- crq call acq
     logic crq_acq_cal; // crq drive
@@ -172,6 +182,50 @@ module wired_tl_adapter import tl_pkg::*; #(
         fsm_t fsm_q;
         localparam fsm_t S_FREE = 0;
         localparam fsm_t S_INV = 1; // Call inv
+        typedef struct packed {
+            logic [31:0] addr;
+            logic [2:0] parm;
+        } registerd_entrys;
+        registerd_entrys init = '0;
+        registerd_entrys d;
+        registerd_entrys q;
+        always_ff @(posedge clk) begin
+            if(!rst_n) begin
+                d <= init;
+                fsm_q <= S_FREE;
+            end else begin
+                d <= q;
+                fsm_q <= fsm;
+            end
+        end
+        always_comb begin
+            // ALL DRIVEN SIGNAL
+            d = q;
+            fsm = fsm_q;
+            prb_b_ready = '0;
+            prb_inv_parm = PRB_HIT_ADDR_N;
+            prb_inv_addr = q.addr;
+            prb_inv_cal = '0;
+            case (fsm_q)
+                /*S_FREE*/default: begin
+                    prb_b_ready = '1;
+                    if(prb_b_valid) begin
+                        // TODO: ADD RESOURCE PROTECTION, BUT NOT TODAY
+                        fsm = S_INV;
+                        d.addr = prb_b.address;
+                        d.parm = prb_b.parm;
+                    end
+                end
+                S_INV: begin
+                    prb_inv_parm = q.parm == tl_pkg::toB ? PRB_HIT_ADDR_B : PRB_HIT_ADDR_N;
+                    prb_inv_addr = q.addr;
+                    prb_inv_cal = '1;
+                    if(prb_inv_ret) begin
+                        fsm = S_FREE;
+                    end
+                end
+            endcase
+        end
     end
 
     // CPU_Request 状态机 - crq - - write SRAM-DATA
@@ -186,6 +240,20 @@ module wired_tl_adapter import tl_pkg::*; #(
         localparam fsm_t S_COP = 3; // Call inv
         localparam fsm_t S_INV = 4; // Call inv
         localparam fsm_t S_ACQ = 5; // Call acq
+        always_ff @(posedge clk) begin
+            if(!rst_n) begin
+                fsm_q <= S_FREE;
+            end else begin
+                fsm_q <= fsm;
+            end
+        end
+        typedef struct packed {
+            logic     sel_prb; // 响应来自 prb 的请求，否之则是 crq
+            logic [31:0] addr;
+        } registerd_entrys;
+        always_comb begin
+            
+        end
     end
 
     // Invalid     状态机 - inv - C、D - read/write SRAM-TAG, read SRAM-DATA
@@ -195,11 +263,115 @@ module wired_tl_adapter import tl_pkg::*; #(
         fsm_t fsm;
         fsm_t fsm_q;
         localparam fsm_t S_FREE = 0;
-        localparam fsm_t S_ULD = 1; // Call unc
-        localparam fsm_t S_UST = 2; // Call unc
-        localparam fsm_t S_COP = 3; // Call inv
-        localparam fsm_t S_INV = 4; // Call inv
-        localparam fsm_t S_ACQ = 5; // Call acq
+        localparam fsm_t S_RTAG0 = 1; // Read tags request
+        localparam fsm_t S_RTAG1 = 2; // Read tags fetch result
+        localparam fsm_t S_MTAG = 3; // Match tags
+        localparam fsm_t S_WTAG = 4; // Write tag sram
+        localparam fsm_t S_RDAT = 5; // Read data sram
+        localparam fsm_t S_TLC  = 6; // TL-C
+        localparam fsm_t S_TLD  = 7; // TL-D
+        typedef struct packed {
+            logic     prb_sel; // 选择来自 prb 的请求
+            logic [31:0] addr;
+            inv_parm_e  parm;
+            cache_tag_t [3:0] tags;
+            logic [3:0] mask;
+            logic [1:0] perm;
+        } registerd_entrys;
+        registerd_entrys init = '0;
+        registerd_entrys q;
+        registerd_entrys d;
+        always_ff @(posedge clk) begin
+            if(!rst_n) begin
+                q <= init;
+            end else begin
+                q <= d;
+            end
+        end
+        always_comb begin
+            fsm = fsm_q;
+            d = q;
+            prb_inv_ret = '0;
+            crq_inv_ret = '0;
+            crq_inv_sel = '0;
+            inv_c_valid = '0;
+            inv_c = '0;
+            inv_data_valid = '0;
+            inv_data_addr = '0;
+            inv_data_way = '0;
+            inv_tag_valid = '0;
+            inv_tag_we = '0;
+            inv_tag_set = '0;
+            inv_tag = '0;
+            case (fsm_q)
+                /*S_FREE*/default: begin
+                    if(prb_inv_ret) begin // 这个优先级比较高哦
+                        d.prb_sel = '1;
+                        d.addr = prb_inv_addr;
+                        d.parm = prb_inv_parm;
+                        if(d.parm inside {IDX_INV, IDX_INIT}) begin
+                            d.mask = d.addr[15:12];
+                        end else begin
+                            d.mask = '0;
+                        end
+                    end
+                end
+                S_RTAG0: begin
+                    inv_tag_valid = '1;
+                    inv_tag_set = q.addr[11:4];
+                    if(inv_tag_ready) begin
+                        fsm = S_RTAG1;
+                    end
+                end
+                S_RTAG1: begin
+                    d.tags = inv_rtag;
+                    // RD_ALLOC 不需要 match，只需要找空行
+                    if(q.parm inside {RD_ALLOC, IDX_INV, IDX_INIT}) begin
+                        fsm = S_WTAG;
+                    end else begin
+                        fsm = S_MTAG;
+                    end
+                end
+                S_MTAG: begin
+                    for(integer i = 0 ; i < 4 ; i += 1) begin
+                        if(q.tags[i].rp && q.tags[i].p == q.addr[31:12]) begin
+                            d.mask[i] = '1; // hit logic 
+                            d.perm = {q.tags[i].rp, q.tags[i].wp};
+                            if(q.parm = WR_ALLOC) begin
+                                fsm = S_FREE;
+                                crq_inv_ret = '1;
+                                crq_inv_sel = i[1:0];
+                            end
+                        end
+                    end
+                    if(!crq_inv_ret) begin
+                        fsm = S_WTAG;
+                    end
+                end
+                S_WTAG: begin
+                    // WR_ALLOC | RD_ALLOC
+                    // PRB_HIT_ADDR_N
+                    // PRB_HIT_ADDR_B
+                end
+            endcase
+        end
+    end
+
+    // Acquire     状态机 - acq - A、D、E - write SRAM-TAG, write SRAM-DATA
+    if(1) begin : acq_fsm
+        /* PROBE 状态机，状态定义 */
+        typedef logic[2:0] fsm_t;
+        fsm_t fsm;
+        fsm_t fsm_q;
+        localparam fsm_t S_FREE = 0;
+        localparam fsm_t S_TLA  = 1; // Issue A request
+        localparam fsm_t S_TLD  = 2; // Waiting for D response, and lock B
+        localparam fsm_t S_WRAM = 3; // Write data&tag sram
+        localparam fsm_t S_TLE  = 4; // Issue E response
+        typedef struct packed {
+            logic [31:0] addr;
+            logic [2:0] parm;
+        } registerd_entrys;
     end
 
     // SRAM 仲裁器，固定优先级
