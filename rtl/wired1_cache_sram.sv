97fe713e84cdcc38c2659c47f57a7d1fedae5085
dofingert <dofingert@gmail.com>
2024-03-26 22:30:36 +0800

    修正 cache sram 模型，输出时执行 align 操作

--- rtl/wired1_cache_sram.sv
+++ rtl/wired1_cache_sram.sv
@@ -8,7 +8,6 @@ module wired_cache_sram#(
 
     input  logic [11:0]               p_addr_i, // CPU 侧访问地址，返回所有路（共四路）上的数据
     output logic [3:0][WORD_SIZE-1:0] p_rdata_o,
-    output logic [1:0]                p_sll_o,  // CPU 侧访问需要的偏移（注意：施加在 hit 上，循环左移 rhit = (hit >> p_sll_o) | (hit << (4-p_sll_o)) ）
 
     input  logic [1:0]  m_way_i,
     input  logic [11:0] m_addr_i,                // 状态机侧访问地址，读写某 way 的整行
@@ -19,127 +18,101 @@ module wired_cache_sram#(
 
   // 四路 SRAM 控制线
   // 为 1024x32b / 512x64b  的双端口类型
-  localparam ADDR_LENGTH = $clog2(4096*8/WORD_SIZE)
-             logic [3:0][ADDR_LENGTH-1:0] p_sram_addr, m_sram_addr;
-  logic [3:0][WORD_SIZE/8-1:0] m_sram_strb;
-
-  logic [3:0][WORD_SIZE-1:0] p_sram_rdata, m_sram_rdata;
-  logic [3:0][WORD_SIZE-1:0] m_sram_wdata;
-  // 例化 sram
-  for(genvar i = 0 ; i < 4 ; i += 1)
-  begin : GEN_SRAM
-    wire conflict_fire = (|m_sram_strb[i]) && m_sram_addr[i] == p_sram_addr[i];
-    reg conflict_fire_q;
+  // 对于 P 端口，直接原样输出就行，输入也直连 p_addr_i 就行
+  // 对于 M 端口，输出时需要预先做好移位工作
+  // 对于每一路的访问地址低位，也需要根据 way 计算。
+  logic [3:0][WORD_SIZE-1:0] raw_pdata;
+  logic [3:0][WORD_SIZE-1:0] raw_rdata;
+  for(genvar b = 0 ; b < 4 ; b += 1)
+  begin : sram_gen
+    // 本地生成 m 口的访问端口
+    wire [11:0] m_addr;
+    wire [3:0]  m_strb;
+    wire [WORD_SIZE/8-1:0] m_strb;
+    wire [WORD_SIZE-1:0]   m_wdata;
+    if(WORD_SIZE == 32)
+    begin
+      assign m_addr[11:4] = m_addr_i[11:4];
+      assign m_addr[3:2]  = m_way_i - b[1:0];
+      assign m_addr[1:0]  = '0;
+      assign m_strb  = m_wstrb_i[m_addr[3:2]];
+      assign m_wdata = m_wdata_i[m_addr[3:2]];
+    end
+    else
+    begin
+      assign m_addr[11:4] = m_addr_i[11:4];
+      assign m_addr[3]    = m_way_i[0] ^ b[0];
+      assign m_addr[2:0]  = '0;
+      assign m_strb[3:0]    = {4{b[1] == m_way_i[1]}} & m_wstrb_i[{m_addr[3],1'b0}];
+      assign m_wdata[31:0]  = m_wdata_i[{m_addr[3],1'b0}];
+      assign m_strb[7:4]    = {4{b[1] == m_way_i[1]}} & m_wstrb_i[{m_addr[3],1'b1}];
+      assign m_wdata[63:32] = m_wdata_i[{m_addr[3],1'b1}];
+    end
+    wire conflict = p_addr_i[11:$clog2(WORD_SIZE/8)] == m_addr[11:$clog2(WORD_SIZE/8)];
+    logic conflict_q;
     always_ff @(posedge clk)
     begin
-      conflict_fire_q <= conflict_fire;
+      conflict_q <= conflict;
     end
-    logic [WORD_SIZE-1:0] p_sram_rdata_raw;
-    assign p_sram_rdata[i] = conflict_fire_q ? m_sram_rdata[i] : p_sram_rdata[i];
+    logic [WORD_SIZE-1:0] rdata_p0, rdata_p1;
+    assign raw_pdata[b] = conflict_q ? rdata_p1 : rdata_p0;
+    assign raw_rdata[b] = rdata_p1;
     wired_dpsram # (
                    .DATA_WIDTH(WORD_SIZE),
-                   .DATA_DEPTH(4096*8/WORD_SIZE),
+                   .DATA_DEPTH(4096*8/WORD_SIZE), // 512 / 1024
                    .BYTE_SIZE(8)
                  )
                  wired_dpsram_inst (
                    .clk0(clk),
                    .rst_n0(rst_n),
-                   // RO Port in verilator
-                   .addr0_i(p_sram_addr[i]),
-                   .en0_i(!conflict_fire),
+                   .addr0_i(p_addr_i[11:$clog2(WORD_SIZE/8)]),
+                   .en0_i(!conflict),
                    .we0_i('0),
                    .wdata0_i('0),
-                   .rdata0_o(p_sram_rdata_raw),
+                   .rdata0_o(rdata_p0),
                    .clk1(clk),
                    .rst_n1(rst_n),
-                   .addr1_i(m_sram_addr[i]),
+                   .addr1_i(m_addr[11:$clog2(WORD_SIZE/8)]),
                    .en1_i('1),
-                   .we1_i(m_sram_strb[i]),
-                   .wdata1_i(m_sram_wdata[i]),
-                   .rdata1_o(m_sram_rdata[i])
+                   .we1_i(m_strb),
+                   .wdata1_i(m_wdata),
+                   .rdata1_o(rdata_p1)
                  );
   end
 
-  // SRAM 只读端口
-  // 注意 SRAM 由于存在一个特别的 BANK 设计
-  // |    | B0 | B1 | B2 | B3 |
-  // | A0 | W0 | W1 | W2 | W3 |
-  // | A1 | W1 | W2 | W3 | W0 |
-  // | A2 | W2 | W3 | W0 | W1 |
-  // | A3 | W3 | W0 | W1 | W2 |
-  // 实际访存的 bank_mask == shuttle(hit >> addr[3:2])
-  // 写回/重填时，bank_addr[i][3:2] = way_id - i;
-  // 写回：data_128 = {B0, B1, B2, B3} >> (3-way_id);
-  // 重填：data_32[i] = data_128[way_id - i];
-
-  // icache 的情况？
-  // |    | B0 | B1 | B2 | B3 |
-  // | A0 | W0 | W1 | W2 | W3 |
-  // |    | W0 | W1 | W2 | W3 |
-  // | A2 | W2 | W3 | W0 | W1 |
-  // |    | W2 | W3 | W0 | W1 |
-  // 实际访存的 bank_mask == shuttle(hit >> addr[3:2])
-  // 写回/重填时，bank_addr[i][3:2] = way_id - i;
-  // 写回：data_256 = {B0, B1, B2, B3} >> (3-way_id);
-  // 重填：data_64[i] = data_256[way_id - i];
-  // 即完全一致，只是总线拓宽到 256b
-  // 输出逻辑
-  // P 口不在这里对齐
-  assign p_rdata_o = p_sram_rdata;
-  reg  [1:0] p_sll_q;
-  always_ff @(posedge clk)
-  begin
-    p_sll_q <= WORD_SIZE == 64 ? {p_addr_i[3], 1'b0} : {p_addr_i[3:2]};
-  end
-  assign p_sll_o = p_sll_q;
-  // M 口在这里对齐
-  reg [1:0] m_way_q;
-  always_ff @(posedge clk)
-  begin
+  // 输出移位
+  logic [1:0] m_way_q;
+  always_ff @(posedge clk) begin
     m_way_q <= m_way_i;
   end
   if(WORD_SIZE == 32)
   begin
-    for(genvar b = 0 ; b < 4 ; b += 1)
+    for(genvar s = 0 ; s < 4 ; s += 1)
     begin
-      wire [1:0] m_sel = m_way_q-b[1:0];
-      assign m_rdata_o[b] = m_sram_rdata[m_sel];
+      wire [1:0] sft = m_way_q - s[1:0];
+      assign m_rdata_o[s] = raw_rdata[sft];
     end
   end
   else
   begin
-    for(genvar b = 0 ; b < 4 ; b += 2)
+    for(genvar s = 0 ; s < 4 ; s += 2)
     begin
-      wire [1:0] m_sel = m_way_q-b[1:0];
-      assign m_rdata_o[b]   = m_sram_rdata[m_sel][31:0];
-      assign m_rdata_o[b+1] = m_sram_rdata[m_sel][63:32];
+      wire lr_sel = m_way_q[1];
+      wire sft = m_way_q[0] ^ s[1];
+      assign m_rdata_o[{s[1], 1'd1}] = raw_rdata[{lr_sel, sft}][63:32];
+      assign m_rdata_o[{s[1], 1'd0}] = raw_rdata[{lr_sel, sft}][31:0];
     end
   end
-
-  // 输入逻辑
-  // P 口很直球
-  for(genvar b = 0 ; b < 4 ; b += 1)
-  begin
-    assign p_sram_addr[b] = p_addr_i[11:12-ADDR_LENGTH];
+  logic [1:0] p_s_q;
+  always_ff @(posedge clk) begin
+    p_s_q <= p_addr_i[3:2];
   end
-  // M 口要绕一些
-  for(genvar b = 0 ; b < 4 ; b += 1)
-  begin
-    if(WORD_SIZE == 32)
-    begin
-      wire [1:0] m_sel = m_way_i-b[1:0];
-      assign m_sram_addr[b] = {m_addr_i[11:4], m_sel};
-      assign m_sram_wdata[b] = m_wdata_i[m_sel];
-      assign m_sram_strb[b] = m_wstrb_i[m_sel];
-    end
-    else
-    begin
-      wire m_sel = m_way_i[1]-b[1];
-      assign m_sram_addr[b] = {m_addr_i[11:4], m_sel};
-      assign m_sram_wdata[b][31:0] = m_wdata_i[{m_sel, 1'b0}];
-      assign m_sram_wdata[b][63:32] = m_wdata_i[{m_sel, 1'b1}];
-      assign m_sram_strb[b][3:0] = m_wstrb_i[{m_sel, 1'b0}] && b[1] == m_way_i[1];
-      assign m_sram_strb[b][7:4] = m_wstrb_i[{m_sel, 1'b1}] && b[1] == m_way_i[1];
+  for(genvar w = 0 ; w < 4 ; w += 1) begin
+    if(WORD_SIZE == 32) begin
+      wire [1:0] sft = w[1:0] - p_s_q;
+      assign p_rdata_o[w] = raw_pdata[sft];
+    end else begin
+      assign p_rdata_o[w] = raw_pdata[{w[1], w[0] ^ p_s_q[1]}];
     end
   end
 
--- rtl/wired2_icache.sv
+++ rtl/wired2_icache.sv
@@ -3,7 +3,7 @@
 
 module wired_icache #(
     parameter integer PACKED_SIZE = 32
-)(
+  )(
     `_WIRED_GENERAL_DEFINE,
 
     // CPU FETCH 请求接口
@@ -14,10 +14,12 @@ module wired_icache #(
     input  logic [PACKED_SIZE-1 : 0] f_pkg_i,
 
     // CACHE OP 请求端口
-    input  logic                   c_valid_i,
-    output logic                   c_ready_o,
-    input  logic            [31:0]  c_addr_i,
-    input  inv_parm_e               c_parm_i,
+    // 很简单，这就是不需要的
+    // ibarrier 一定会使后端写入在前端可见。
+    // input  logic                   c_valid_i,
+    // output logic                   c_ready_o,
+    // input  logic            [31:0]  c_addr_i,
+    // input  inv_parm_e               c_parm_i,
 
     // CPU FETCH 请求返回接口
     output logic                   f_valid_o,
@@ -26,6 +28,7 @@ module wired_icache #(
     output logic            [31:0]    f_pc_o,
     output logic       [1:0][31:0]  f_inst_o,
     output logic [PACKED_SIZE-1 : 0] f_pkg_o,
+    output fetch_excp_t             f_excp_o, // 取值错误，显然两条指令是一致的
 
     // 到总线侧的请求接口
     output lsu_bus_req_t  bus_req_o, // TODO
@@ -45,82 +48,114 @@ module wired_icache #(
     input  logic [1:0]        p_sll_i,
 
     // 无效化端口
-    input  logic              flush_i
-);
-    logic f1_skid_busy_q; // F1 级别进入 skid 状态的信号
-    assign f_ready_o = (!f1_skid_busy_q) && (!c_valid_i);
-    assign c_raedy_o = !f1_skid_busy_q;
-    // F2 段到 f1 握手
-    logic f1_f2_valid, f1_f2_ready; // 握手信号，纯组合逻辑驱动
-    typedef struct packed {
-        logic [31:0] addr;
-        inv_parm_e   parm;
-        logic [1:0]  mask;
-        logic     uncache;
+    input  logic              flush_i   // 所有管线中的指令都会被无效化
+  );
+  logic f1_skid_busy_q; // F1 级别进入 skid 状态的信号
+  assign f_ready_o = (!f1_skid_busy_q) && (!c_valid_i);
+  assign c_raedy_o = !f1_skid_busy_q;
+  // F2 段到 f1 握手
+  logic f1_f2_valid, f1_f2_ready; // 握手信号，纯组合逻辑驱动
+  typedef struct packed {
+            logic [31:0] addr;
+            inv_parm_e   parm;
+            logic [1:0]  mask;
+            logic     uncache;
 
-        logic [PACKED_SIZE - 1 : 0] pkg; // 时刻携带传入信息
+            logic [PACKED_SIZE - 1 : 0] pkg; // 时刻携带传入信息
 
-        cache_tag_t [3:0]  tag;
-        logic [3:0][31:0] data;
-        logic inv; logic ppi; logic ale; logic tlbr; // TLB EXCP
-    } f1_pack_t;
-    f1_pack_t f1_raw, f1_sel, f1; // 组合逻辑
-    f1_pack_t f1_skid_q; // 寄存器堆
-    logic [3:0] f1_hit;  // 纯组合逻辑，基于 f1 信号
-    always_ff @(posedge clk) begin
-        if(!f1_skid_busy_q) begin
-            f1_skid_q <= f1; // 每拍都打
+            cache_tag_t [3:0]  tag;
+            logic [3:0][63:0] data;
+            logic inv; logic ppi; logic ale; logic tlbr; // TLB EXCP
+          } f1_pack_t;
+  f1_pack_t f1_raw, f1_sel, f1; // 组合逻辑
+  f1_pack_t f1_skid_q; // 寄存器堆
+  logic [3:0] f1_hit;  // 纯组合逻辑，基于 f1 信号
+  always_ff @(posedge clk)
+  begin
+    if(!f1_skid_busy_q)
+    begin
+      f1_skid_q <= f1; // 每拍都打
+    end
+  end
+  assign f1_sel = f1_skid_busy_q ? f1_skid_q : f1_raw;
+
+  // SNOP 逻辑开始
+  always_comb
+  begin
+    f1 = f1_sel;
+    // TODO: CHECKME
+    // 对 DATA SRAM 的 snoop
+    if(snoop_i.daddr[11:4] == f1.paddr[11:4])
+    begin // 是同一个 Cache line
+      f1.data[snoop_i.dway] = {
+               gen_mask_word(f1.data[snoop_i.dway][63:32],
+                             snoop_i.d[{f1.paddr[3], 1'b1}],
+                             snoop_i.dstrb[{f1.paddr[3], 1'b1}]),
+               gen_mask_word(f1.data[snoop_i.dway][31:0],
+                             snoop_i.d[{f1.paddr[3], 1'b0}],
+                             snoop_i.dstrb[{f1.paddr[3], 1'b0}])};
+    end
+    if(snoop_i.taddr[11:4] == f1.paddr[11:4])
+    begin // 是同一个 Cache line
+      for(integer w = 0 ; w < 4 ; w += 1)
+      begin // 逐路检查
+        if(snoop_i.twe[w])
+        begin
+          f1.tag[w] = snoop_i.tag[w];
         end
-    end
-    assign f1_sel = f1_skid_busy_q ? f1_skid_q : f1_raw;
-    
-    // SNOP 逻辑开始
-    always_comb begin
-        f1 = f1_sel;
-        // TODO: SNOOP SRAM SIGNALS
+      end
     end
 
-    // F1_RAW 逻辑开始
-    always_comb begin
-        f1_raw = '0;
-        // TODO: RAW GENERATION LOGICS
-    end
+  end
 
-    // HIT 逻辑开始
-    for(genvar w = 0 ; w < 4 ; w += 1) begin
-        assign f1_hit[w] = '0; // 生成四路 hit信号。
-        // TODO        
-    end
-    /* F2 段开始 */
-    logic f2_valid_q; // F2 有效寄存器，状态机使用
-    typedef struct packed {
-        logic [31:0] addr;
-        inv_parm_e   parm;
-        logic [1:0]  mask;
-        logic     uncache;
+  // F1_RAW 逻辑开始
+  always_comb
+  begin
+    f1_raw = '0;
+    // TODO: RAW GENERATION LOGICS
+  end
 
-        logic [PACKED_SIZE - 1 : 0] pkg; // 时刻携带传入信息
+  // HIT 逻辑开始
+  for(genvar w = 0 ; w < 4 ; w += 1)
+  begin
+    assign f1_hit[w] = '0; // 生成四路 hit信号。
+    // TODO
+  end
+  /* F2 段开始 */
+  logic f2_valid_q; // F2 有效寄存器，状态机使用
+  typedef struct packed {
+            logic [31:0] addr;
+            inv_parm_e   parm;
+            logic [1:0]  mask;
+            logic     uncache;
 
-        logic [3:0]   hit; // 是否命中信息
-        logic [3:0][31:0] data; // 需要实时更新
-        logic inv; logic ppi; logic ale; logic tlbr; // TLB EXCP
-    } f2_pack_t;
-    typedef enum logic[3:0] {
+            logic [PACKED_SIZE - 1 : 0] pkg; // 时刻携带传入信息
 
-    } fsm_t;
-    fsm_t fsm;
-    fsm_t fsm_q;
-    always_ff @(posedge clk) begin
-        if(!rst_n) begin
-            fsm_q <= '0;
-        end else begin
-            fsm_q <= fsm;
-        end
+            logic [3:0]   hit; // 是否命中信息
+            logic [3:0][31:0] data; // 需要实时更新
+            logic inv; logic ppi; logic ale; logic tlbr; // TLB EXCP
+          } f2_pack_t;
+  typedef enum logic[3:0] {
+
+          } fsm_t;
+  fsm_t fsm;
+  fsm_t fsm_q;
+  always_ff @(posedge clk)
+  begin
+    if(!rst_n)
+    begin
+      fsm_q <= '0;
     end
-    always_comb begin
-        fsm = fsm_q;
-        // 之后是主要状态机
+    else
+    begin
+      fsm_q <= fsm;
     end
+  end
+  always_comb
+  begin
+    fsm = fsm_q;
+    // 之后是主要状态机
+  end
 
 
 endmodule
