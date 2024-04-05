193d7370c00956e07f126e0bb024b216fbaf2da3
dofingert <dofingert@gmail.com>
2024-04-05 20:41:12 +0800

    似乎修正了 需要commit状态机的指令跳转预测错误 时指令被误提交的 bug

--- rtl/wired2_commit.sv
+++ rtl/wired2_commit.sv
@@ -584,9 +584,11 @@ module wired_commit (
                     // 跳转指令处理，注意刷新管线
                     // if(h_entry_q[0].di.jump_inst) begin // 对非访存指令，也可能会误预测。
                     // 特殊处理未命中情况，刷新流水线，重定向控制流
-                    if( (h_entry_q[0].need_jump && (!h_entry_q[0].bpu_predict.taken || h_entry_q[0].bpu_predict.predict_pc != h_entry_q[0].target_addr)) ||
-                       (!h_entry_q[0].need_jump && h_entry_q[0].bpu_predict.taken)) begin
-                        l_commit &= 2'b01;
+                    // 注意：到达这里的指令，可能下一拍不一定正常执行，检查 l_commit[0]
+                    if(  l_commit[0] &&
+                      ((h_entry_q[0].need_jump && (!h_entry_q[0].bpu_predict.taken || h_entry_q[0].bpu_predict.predict_pc != h_entry_q[0].target_addr)) ||
+                      (!h_entry_q[0].need_jump &&   h_entry_q[0].bpu_predict.taken))) begin
+                        l_commit = 2'b01;
                         f_upd.miss = '1;
                         f_upd.need_update = '1;
                         f_upd.redirect = '1;
@@ -699,8 +701,8 @@ module wired_commit (
                         end
                     end
                     // flush / ertn 逻辑 及 idle 逻辑
-                    if(h_entry_q[0].di.priv_inst) begin
-                        l_commit &= 2'b01;
+                    if(l_commit[0] && h_entry_q[0].di.priv_inst) begin
+                        l_commit = 2'b01;
                         f_upd.redirect = '1;
                         f_upd.true_target = h_flushtarget_q;
                         fsm = h_entry_q[0].di.wait_inst ? S_WAIT_INTERRUPT : S_WAIT_FLUSH;
